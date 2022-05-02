c1b96cea0e7d0c0a04b89cf9d902516551a7eabc
Myles Block <myles.block@bison.howard.edu>
2022-05-02 18:52:17 +0000

    Modified main.py to read input directly from the command line for initialization

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import sys
 
-def function(test):
-    print("function works")
 
-function("test")
-for line in sys.stdin:
-    print("Check " + line)
+n = len(sys.argv)
+if n > 1:
+    key = sys.argv[1]
+    print(sys.argv[1])#how to pull arguements after python3 main.py 1
+    print("Enter Caesar Cipher phrase: (type q to stop)")
+    for line in sys.stdin:
+        line = line.rstrip()
+        line = line.upper()
+        print(line)
+
+
+        if ('q' == line.rstrip() or 'Q' == line.rstrip()):
+            break
+else:
+    print("You gave no key...")
+    print("Remember type python3 main.py <key> in execution")
+    print("")
